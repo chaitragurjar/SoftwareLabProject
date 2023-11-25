2380caf6c9647de25c49455105e0d67ca11759cb
chaitragurjar <62434974+chaitragurjar@users.noreply.github.com>
2023-11-25 20:49:59 +0530

    Add files via upload

--- src/image/views.py
+++ src/image/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,redirect
+import os
+import subprocess
 from .form import ImageForm
 from .models import Image
 
@@ -9,8 +11,17 @@ def index(request):
         if form.is_valid():
             form.save()
             obj=form.instance
+            filename=obj.Upload_File.name
+            filepath=obj.Upload_File.path
+            #print("spleeter separate -o output/ "+filepath)
+            #print("filename="+obj.Upload_File.name)
+            status = 1
+            status = os.system('spleeter separate -o media/output/ '+filepath)
+            #print("/output/"+filename+"/vocals.wav")
+            obj.Upload_File.name=filename.split('.')[0]
             return render(request,"index.html",{"obj":obj})
     else:
         form=ImageForm()
     img=Image.objects.all()
+    
     return render(request,"index.html",{"img":img,"form":form})
