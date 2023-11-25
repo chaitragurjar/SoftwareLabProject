75e80628947378ef7ca56c778383482571a3d573
Akanksha Dadhich <40837407+AkankshaDadhich@users.noreply.github.com>
2023-11-26 04:35:48 +0530

    Update bash.sh
    Updated bash.sh

--- bash.sh
+++ bash.sh
@@ -3,10 +3,9 @@
 
 pip install django==4.2.6
 pip show django
-apt install ffmpeg
 pip install spleeter
 git clone https://github.com/chaitragurjar/SoftwareLabProject.git
-cd SoftwareLabProject/
+cd SoftwareLabProject/src
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver
