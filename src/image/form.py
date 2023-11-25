bcab06dd312aff5ee1b4c75006a2c58851ff862a
chaitragurjar <62434974+chaitragurjar@users.noreply.github.com>
2023-11-25 20:50:26 +0530

    Add files via upload

--- src/image/form.py
+++ src/image/form.py
@@ -4,4 +4,8 @@ from .models import Image
 class ImageForm(forms.ModelForm):
     class Meta:
         model=Image
-        fields=("caption","image")
+        fields=('Song_Name','Upload_File')
+        widgets = {
+            'Song_Name': forms.TextInput(attrs={'class':'form-control'}),
+            'Upload_File': forms.ClearableFileInput(attrs={'class':'form-control'}),
+        }
